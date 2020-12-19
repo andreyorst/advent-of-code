a065e88888af79f1d57984ebcfeaaf1aa533dfb9
Andrey Orst <andreyorst@gmail.com>
2020-12-19 13:07:24 +0300

    add part one for day7

--- Clojure/src/aoc2020/core.clj
+++ Clojure/src/aoc2020/core.clj
@@ -261,8 +261,45 @@
     (println "  part two:" (count-simultaneous-answers answers))))
 
 
+(ns day7
+  (:require [clojure.string :as str]))
+
+(defn- parse-bag-spec [spec]
+  (let [main-color-re #"(\w+\s+\w+)\s+bags\s+contain"
+        inner-color-re #"(?:\s+\d+\s+\w+\s+\w+\s+bags?[.,])+"
+        no-bags-re #"(?:\s+no\s+other\s+bags)"
+        bag-re (re-pattern (format "^%s(%s|%s)" main-color-re inner-color-re no-bags-re))
+        [_ color amounts] (re-find bag-re spec)]
+    [color (if (re-find no-bags-re amounts)
+             {}
+             (->> (str/split amounts #"[.,]\s+")
+                  (map #(let [[_ amount color] (re-find #"\s*(\d+)\s+(\w+\s+\w+)" %)]
+                          [color (Integer. amount)]))
+                  (into {})))]))
+
+(defn- read-bag-info [input]
+  (->> input
+       slurp
+       str/split-lines
+       (map parse-bag-spec)
+       (into {})))
+
+(defn- check-bag [[bag-color inner-bags] color bags]
+  (if (inner-bags color)
+    bag-color
+    (map #(check-bag [bag-color (bags (first %))] color bags) inner-bags)))
+
+(defn- count-bags [bags color]
+  (count (into #{} (flatten (map #(check-bag % color bags) bags)))))
+
+(defn handy-haversacks []
+  (println "Day 7 - Handy Haversacks")
+  (let [bags (read-bag-info "../inputs/day7.txt")]
+    (println "  part one:" (count-bags bags "shiny gold"))))
+
+
 (ns aoc2020.core
-  (:require [day1] [day2] [day3] [day4] [day5] [day6])
+  (:require [day1] [day2] [day3] [day4] [day5] [day6] [day7])
   (:gen-class))
 
 (defn -main [& _]
@@ -271,4 +308,5 @@
   (day3/toboggan-trajectory)
   (day4/passport-processing)
   (day5/binary-boarding)
-  (day6/custom-customs))
+  (day6/custom-customs)
+  (day7/handy-haversacks))
