0840e5e3e4e5095be875238b3c481c29494dac9e
Andrey Orst <andreyorst@gmail.com>
2020-12-21 22:45:36 +0300

    add day 11 solution

--- aoc2020.clj
+++ aoc2020.clj
@@ -444,9 +444,101 @@
     (println "  part two:" (adapter-combinations adapters))))
 
 
+(ns day11
+  (:require [clojure.string :as str]))
+
+(defn- read-seat-data [input]
+  (->> input
+       slurp
+       str/split-lines
+       (map #(into [] (str/split % #"")))
+       (into [])))
+
+(defn- occupied? [seat]
+  (if (and seat (= seat "#")) 1 0))
+
+(defn- seat-liberties [x y seats]
+  (+ (occupied? (get-in seats [(inc x) y]))
+     (occupied? (get-in seats [(dec x) y]))
+     (occupied? (get-in seats [x (inc y)]))
+     (occupied? (get-in seats [x (dec y)]))
+     (occupied? (get-in seats [(inc x) (inc y)]))
+     (occupied? (get-in seats [(dec x) (dec y)]))
+     (occupied? (get-in seats [(dec x) (inc y)]))
+     (occupied? (get-in seats [(inc x) (dec y)]))))
+
+(defn- can-occupy? [seat liberty]
+  (and (= seat "L") (= liberty 0)))
+
+(defn- should-leave? [seat liberty max-liberty]
+  (and (= seat "#") (>= liberty max-liberty)))
+
+(defn- occupy-seats [seats]
+  (let [new-state (atom seats)]
+    (dotimes [x (count seats)]
+      (dotimes [y (count (first seats))]
+        (let [seat (get-in seats [x y])
+              liberty (seat-liberties x y seats)]
+          (swap! new-state assoc-in [x y]
+                 (cond
+                   (should-leave? seat liberty 4) "L"
+                   (can-occupy? seat liberty) "#"
+                   :else seat)))))
+    @new-state))
+
+
+(defn- wide-occupied? [x y dir-x dir-y seats]
+  (loop [x (+ dir-x x)
+         y (+ dir-y y)]
+    (if-let [seat (get-in seats [x y])]
+      (cond
+        (= "#" seat) 1
+        (= "L" seat) 0
+        :else (recur (+ x dir-x) (+ y dir-y)))
+      0)))
+
+(defn- seat-wide-liberties [x y seats]
+  (+ (wide-occupied? x y  1  0 seats)
+     (wide-occupied? x y -1  0 seats)
+     (wide-occupied? x y  0  1 seats)
+     (wide-occupied? x y  0 -1 seats)
+     (wide-occupied? x y  1  1 seats)
+     (wide-occupied? x y -1 -1 seats)
+     (wide-occupied? x y -1  1 seats)
+     (wide-occupied? x y  1 -1 seats)))
+
+(defn- wide-occupy-seats [seats]
+  (let [new-state (atom seats)]
+    (dotimes [x (count seats)]
+      (dotimes [y (count (first seats))]
+        (let [seat (get-in seats [x y])
+              liberty (seat-wide-liberties x y seats)]
+          (swap! new-state assoc-in [x y]
+                 (cond
+                   (should-leave? seat liberty 5) "L"
+                   (can-occupy? seat liberty) "#"
+                   :else seat)))))
+    @new-state))
+
+(defn- simulate-crowd [seats occupy-fn]
+  (let [new-state (atom (occupy-seats seats))
+        seats (atom seats)]
+    (while (not= @new-state @seats)
+      (reset! seats @new-state)
+      (reset! new-state (occupy-fn @new-state)))
+    (count (mapcat (fn [row] (filter #(= "#" %) row)) @new-state))))
+
+(defn seating-system []
+  (println "Day 11 - Seating System")
+  (let [seats (read-seat-data "inputs/day11.txt")]
+    (println "  part one:" (simulate-crowd seats occupy-seats))
+    (println "  part two:" (simulate-crowd seats wide-occupy-seats))))
+
+
 (ns aoc2020
   (:require [day1] [day2] [day3] [day4] [day5]
-            [day6] [day7] [day8] [day9] [day10]))
+            [day6] [day7] [day8] [day9] [day10]
+            [day11]))
 
 (day1/report-repair)
 (day2/password-philosophy)
@@ -458,3 +550,4 @@
 (day8/handheld-halting)
 (day9/encoding-error)
 (day10/adapter-array)
+(day11/seating-system)
