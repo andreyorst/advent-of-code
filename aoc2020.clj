ea151952676f2711f813ed44d1f0641a854d2665
Andrey Orst <andreyorst@gmail.com>
2020-12-26 22:51:19 +0300

    add day13 solutions

--- aoc2020.clj
+++ aoc2020.clj
@@ -608,10 +608,60 @@
     (println "  part two:" (move-ship-to-waypoint {:n 0 :e 0 :wn 1 :we 10} directions)))) ;; 18107
 
 
+(ns day13
+  (:require [clojure.string :as str]))
+
+(defn- read-schedule [input]
+  (let [[estimate ids] (-> input
+                           slurp
+                           (str/split-lines))]
+    {:estimate (Integer. estimate)
+     :ids (->> (str/split ids #",")
+               (map #(if (re-find #"\d+" %) (Integer. %) (keyword %))))}))
+
+(defn- earliest-bus [{:keys [estimate ids]}]
+  (let [[id times] (->> ids
+                        (filter pos-int?)
+                        (map #(do [% (int (/ estimate %))]))
+                        (sort (fn [[_ a] [_ b]] (< a b)))
+                        first)
+        timestamp (if (< (* id times) estimate)
+                    (* id (inc times))
+                    (* id times))]
+    (* (- timestamp estimate) id)))
+
+(defn- continious-bus-departures [{:keys [ids]}]
+  (let [time-slots (->> ids
+                        (map-indexed vector)
+                        (filter (fn [[_ id]] (pos-int? id))))
+        #_#_ id (second (first time-slots))]
+    ;; Works for examples, but too slow for the real input,
+    ;; approximately taking about two hours to compute.
+    #_(loop [timestamp id]
+        (if (every? (fn [[delta id]]
+                      (zero? (mod (+ timestamp delta) id)))
+                    time-slots)
+          timestamp
+          (recur (+ timestamp id))))
+    (->> time-slots
+         (reduce (fn [[timestamp next-time] [delta id]]
+                   (if (zero? (mod (+ timestamp delta) id))
+                     [timestamp (* next-time id)]
+                     (recur [(+ timestamp next-time) next-time]
+                            [delta id]))))
+         first)))
+
+(defn shuttle-search []
+  (println "Day 13 - Shuttle Search")
+  (let [schedule (read-schedule "inputs/day13.txt")]
+    (println "  part one:" (earliest-bus schedule))                ;; 6559
+    (println "  part two:" (continious-bus-departures schedule)))) ;; 626670513163231
+
+
 (ns aoc2020
   (:require [day1] [day2] [day3] [day4] [day5]
             [day6] [day7] [day8] [day9] [day10]
-            [day11] [day12]))
+            [day11] [day12] [day13]))
 
 (day1/report-repair)
 (day2/password-philosophy)
@@ -625,3 +675,4 @@
 (day10/adapter-array)
 (day11/seating-system)
 (day12/rain-risk)
+(day13/shuttle-search)
