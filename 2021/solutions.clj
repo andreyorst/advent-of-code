e15f9584c338adfceaddd72056277bb78e0163df
Andrey Listopadov <andreyorst@gmail.com>
2021-12-02 09:50:21 +0300

    add day2 solution

--- 2021/solutions.clj
+++ 2021/solutions.clj
@@ -1,11 +1,24 @@
+(ns aoc-commons)
+
+(defn parse-long [s]
+  (if (string? s)
+    (try
+      (Long/valueOf s)
+      (catch NumberFormatException _
+        nil))
+    (throw (IllegalArgumentException.
+            (format "Expected string, got %s" (some-> s class .getName))))))
+
+
 (ns day1
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc-commons :refer [parse-long]]))
 
 (defn- read-input []
   (->> "inputs/day1"
        slurp
        str/split-lines
-       (map #(Integer/parseInt %))))
+       (map parse-long)))
 
 (defn- part-1 [input]
   (->> input
@@ -26,8 +39,57 @@
     (println "  part one:" (part-1 input))   ; 1215
     (println "  part two:" (part-2 input)))) ; 1150
 
+
+(ns day2
+  (:require [clojure.string :as str]
+            [aoc-commons :refer [parse-long]]))
 
+(defn read-input []
+  (->> "inputs/day2"
+       slurp
+       str/split-lines
+       (map #(str/split % #"\s+"))
+       (map #(update % 1 parse-long))))
+
+(defmulti move
+  "Dispatching on `direction`. Returning the result applicable by
+  `reduce`."
+  (fn [_ [direction _]] direction))
+
+(defmethod move "forward" [[x y] [_ amount]] [(+ x amount) y])
+(defmethod move "up" [[x y] [_ amount]] [x (- y amount)])
+(defmethod move "down" [[x y] [_ amount]] [x (+ y amount)])
+
+(defn part-1 [input]
+  (->> input
+       (reduce move [0 0])
+       (apply *)))
+
+(defn move-and-aim
+  "`case` based version of move function.
+  Does essentially the same thing as `move`, just works accordingly to
+  part 2 rules, and written in a different style."
+  [[x y aim] [direction amount]]
+  (case direction
+    "up" [x y (- aim amount)]
+    "down" [x y (+ aim amount)]
+    "forward" [(+ x amount) (+ y (* aim amount)) aim]))
+
+(defn part-2 [input]
+  (->> input
+       (reduce move-and-aim [0 0 0])
+       (take 2)
+       (apply *)))
+
+(defn dive! []
+  (println "Day 2 - Dive!")
+  (let [input (read-input)]
+    (println "  part one:" (part-1 input))   ; 2036120
+    (println "  part two:" (part-2 input)))) ; 2015547716
+
+
 (ns aoc2021
-  (:require [day1]))
+  (:require day1 day2))
 
 (day1/sonar-sweep)
+(day2/dive!)
