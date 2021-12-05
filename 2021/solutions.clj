129db36e465cac46eb497f57745f7d1546b49f2c
Andrey Listopadov <andreyorst@gmail.com>
2021-12-05 19:45:55 +0300

    submit solution for day 5

--- 2021/solutions.clj
+++ 2021/solutions.clj
@@ -182,18 +182,18 @@
     {:draws (parse-draws (first lines))
      :boards (parse-boards (drop 1 lines))}))
 
-(defn mark-board [number board]
+(defn- mark-board [number board]
   (mapv #(mapv (fn [x] (if (= number x) nil x)) %) board))
 
-(defn rotate [board]
+(defn- rotate [board]
   (apply mapv vector board))
 
-(defn check-board [board]
+(defn- check-board [board]
   (when (or (some (partial every? nil?) board)
             (some (partial every? nil?) (rotate board)))
     board))
 
-(defn find-winning-board [draws boards]
+(defn- find-winning-board [draws boards]
   (loop [[number & draws] draws
          boards boards]
     (let [boards (mapv (partial mark-board number) boards)]
@@ -203,19 +203,19 @@
         (recur draws
                boards)))))
 
-(defn calculate-result [{:keys [win-number board]}]
+(defn- calculate-result [{:keys [win-number board]}]
   (* win-number
      (->> board
           flatten
           (filter number?)
           (apply +))))
 
-(defn part-1 [{:keys [draws boards]}]
+(defn- part-1 [{:keys [draws boards]}]
   (->> boards
        (find-winning-board draws)
        calculate-result))
 
-(defn find-last-winning-board [draws boards]
+(defn- find-last-winning-board [draws boards]
   (loop [[number & draws] draws
          boards boards]
     (let [boards (mapv (partial mark-board number) boards)
@@ -226,7 +226,7 @@
         (recur draws
                non-winning)))))
 
-(defn part-2 [{:keys [draws boards]}]
+(defn- part-2 [{:keys [draws boards]}]
   (->> boards
        (find-last-winning-board draws)
        calculate-result))
@@ -238,10 +238,91 @@
     (println "  part two:" (part-2 input)))) ; 13884
 
 
+(ns day5
+  (:require [aoc-commons :refer [parse-long]]
+            [clojure.string :as str]))
+
+(defn- parse-coordinates [line]
+  (let [[_ x1 y1 x2 y2] (re-matches #"(\d+),(\d+)\s+->\s+(\d+),(\d+)" line)]
+    [[(parse-long x1) (parse-long y1)]
+     [(parse-long x2) (parse-long y2)]]))
+
+(defn- read-input []
+  (->> "inputs/day5"
+       slurp
+       str/split-lines
+       (map parse-coordinates)))
+
+(defn- keep-non-diagonal [coordinates]
+  (filter (fn [[[x1 y1] [x2 y2]]] (or (= x1 x2) (= y1 y2))) coordinates))
+
+(defn- build-line [[[x1 y1] [x2 y2]]]
+  (let [[x1 x2] (sort [x1 x2])
+        [y1 y2] (sort [y1 y2])]
+    (for [x (range x1 (inc x2))
+          y (range y1 (inc y2))]
+      [x y])))
+
+(defn- mark-line [field line]
+  (let [fr (frequencies line)]
+    (merge-with (fnil + 1) field fr)))
+
+(defn- mark-lines [field lines]
+  (reduce mark-line field lines))
+
+(defn- create-field [input]
+  (->> input
+       keep-non-diagonal
+       (map build-line)
+       (mark-lines {})))
+
+(defn- render [size points]
+  (let [field (vec (repeat 10 (vec (repeat 10 "."))))]
+    (->> points
+         (reduce (fn [field [[x y] val]]
+                   (assoc-in field [x y] (str val)))
+                 field)
+         (apply map vector)
+         (map str/join)
+         (str/join "\n")
+         println)))
+
+(defn- part-1 [input]
+  (->> (create-field input)
+       vals
+       (filter #(> % 1))
+       count))
+
+(defn- build-diagonale-line [[[x1 y1] [x2 y2]]]
+  (when (= (Math/abs (- x1 x2)) (Math/abs (- y1 y2)))
+    (let [xdir (if (> 0 (- x2 x1)) -1 1)
+          ydir (if (> 0 (- y2 y1)) -1 1)]
+      (loop [line [[x1 y1]]]
+        (let [[x y] (last line)]
+          (if (and (not= x x2) (not= y y2))
+            (recur (conj line [(+ x xdir) (+ y ydir)]))
+            line))))))
+
+(defn- part-2 [input]
+  (->> input
+       (keep build-diagonale-line)
+       (mark-lines (create-field input))
+       vals
+       (filter #(> % 1))
+       count))
+
+(defn hydrothermal-venture []
+  (println "Hydrothermal Venture")
+  (let [input (read-input)]
+    (println "  part one:" (part-1 input))   ; 5280
+    (println "  part two:" (part-2 input)))) ; 16716
+
+
 (ns aoc2021
-  (:require day1 day2 day3 day4))
+  (:require day1 day2 day3 day4 day5))
 
 (day1/sonar-sweep)
 (day2/dive!)
 (day3/binary-diagnostic)
 (day4/giant-squid)
+(day5/hydrothermal-venture)
