252ec733614bb8a2a697b4a4546fea053f034aec
Andrey Listopadov <andreyorst@gmail.com>
2021-12-20 21:35:38 +0300

    day 20 simplifications

--- 2021/src/day20.clj
+++ 2021/src/day20.clj
@@ -16,25 +16,26 @@
     {:enhance (mapv #(if (= % \#) 1 0) enh)
      :image (img->coords img)}))
 
-(defn- enhancement-pos [{:keys [image enhance]} [x y] step]
-  (let [default (cond (= step 0) 0
-                      (even? step) (peek enhance)
-                      :else (first enhance))]
-    (-> (for [ydir [-1 0 1]
-              xdir [-1 0 1]]
-          (get image [(+ x xdir) (+ y ydir)] default))
-        str/join
-        (Long/parseLong 2))))
+(defn- enhancement [{:keys [image enhancement]} [x y] default]
+  (nth (-> (for [y [(dec y) y (inc y)]
+                 x [(dec x) x (inc x)]]
+             (get image [x y] default))
+           str/join
+           (Long/parseLong 2))
+       enhancement))
 
 (defn- enhance [{:keys [enhance image] :as img} step]
   (let [points (keys image)
         [min-x min-y] (first points)
-        [max-x max-y] (last points)]
+        [max-x max-y] (last points)
+        default (cond (= step 0) 0
+                      (even? step) (peek enhance)
+                      :else (first enhance))]
     {:image (into
              (sorted-map)
              (for [x (range (dec min-x) (+ max-x 2))
                    y (range (dec min-y) (+ max-y 2))]
-               [[x y] (nth enhance (enhancement-pos img [x y] step))]))
+               [[x y] (enhancement img [x y] default)]))
      :enhance enhance}))
 
 (defn- part-1 [input]
@@ -54,21 +55,3 @@
   (let [input (read-input)]
     (println "  part one:" (part-1 input))   ; 5619
     (println "  part two:" (part-2 input)))) ; 20122
-
-#_
-(defn render [image]
-  (let [points (sort (keys image))
-        [min-x min-y] (first points)
-        [max-x max-y] (last points)
-        adj-x (if (< min-x 0) (- min-x) 0)
-        adj-y (if (< min-y 0) (- min-y) 0)
-        field (into [] (repeat (inc (+ max-y adj-y)) (into [] (repeat (inc (+ max-x adj-x)) "."))))]
-    (->> (keys image)
-         (reduce (fn [field [x y]]
-                   (assoc-in field [(+ adj-y y) (+ adj-x x)]
-                             (if (= (image [x y]) 1) "#" ".")))
-                 field)
-         (map str/join)
-         (str/join "\n")
-         println)
-    (println)))
