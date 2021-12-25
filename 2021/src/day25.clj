2b600d306f9e1110058a2f1c9fba5ed0bc578632
Andrey Listopadov <andreyorst@gmail.com>
2021-12-25 21:21:57 +0300

    add day25 solution

---
+++ 2021/src/day25.clj
@@ -0,0 +1,40 @@
+(ns day25
+  (:require [aoc-commons :refer [slurp-lines transpose]]))
+
+(defn read-input []
+  (->> "inputs/day25"
+       slurp-lines
+       (mapv vec)))
+
+(defn move [kind row]
+  (let [len (count row)]
+    (reduce-kv
+     (fn [row' i c]
+       (if (= c kind)
+         (let [pos (mod (inc i) len)]
+           (if (= (nth row pos) \.)
+             (assoc row' i \. pos kind)
+             row'))
+         row'))
+     row row)))
+
+(defn step [world]
+  (->> world
+       (mapv (partial move \>))
+       transpose
+       (mapv (partial move \v))
+       transpose))
+
+(defn part-1 [input]
+  (reduce (fn [world i]
+            (let [world' (step world)]
+              (if (= world' world)
+                (reduced i)
+                world')))
+          input (drop 1 (range))))
+
+(defn run [& _]
+  (println "Day 25 - Sea Cucumber")
+  (let [input (read-input)]
+    (println "  part one:" (part-1 input))
+    #_(println "  part two:" (part-2 input))))
